a5df8aa2191c3edffb3faeec2ca50f65603cf47b
Madhesh <waranmadhesh24@gmail.com>
2026-02-25 15:45:57 +0530

    fix: automate deployment with GitHub Actions

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BinaryRainBackground />
-      <BrowserRouter>
+      <BrowserRouter basename="/Madheshwaran-Portfolio/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
